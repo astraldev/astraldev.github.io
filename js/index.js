830af608ad1ca566f22ba73017187e4c8268ef37
astraldev <61796351+astraldev@users.noreply.github.com>
2021-08-07 05:45:02 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -8,29 +8,76 @@ function load(){
 }
 
 function onLink(obj) {
-  children = obj.parentNode.parentElement.children; // 
-  var i = 0;
-  for (; i < children.length; i++) 
-    children[i].children[0].id = ""
-  obj.id = "active";
+  children = document.querySelector("#menu-list").children; // 
   nm = obj.getAttribute("href").replace("#", "")
+  var i = 0;
+  for (; i < children.length; i++) {
+    var attr = children[i].children[0]
+    try{
+      attr = attr.getAttribute("href").replace("#", "")
+      if(nm == attr){
+        children[i].children[0].id = "active"
+        continue  
+      }
+    }catch(e){}
+    children[i].children[0].id = ""
+  }
+  obj.id = "active";
   console.log(nm)
   section = document.querySelector(`a[name=${nm}]`)
-  i = 0
   children = section.parentNode.parentNode.children;
   for (i; i < children.length; i++) 
-    children[i].classList.add("hidden")
+  children[i].classList.add("hidden")
+  
   var cls_list = section.parentNode.classList
   if (cls_list.contains("hidden")) cls_list.remove("hidden")
   section.parentNode.classList = cls_list
-  var main_menu = document.getElementById("sm-menu-list");
-  main_menu.classList.add("hidden")
-  console.log(main_menu)
+  window.scrollTo(0,0);
+
+}
+function onLink_sm(obj){
+  children = document.querySelector("#sm-menu-list").children; // 
+  nm = obj.getAttribute("href").replace("#", "")
+  var i = 0;
+  for (; i < children.length; i++) {
+    var attr = children[i].children[0]
+    try{
+      attr = attr.getAttribute("href").replace("#", "")
+      if(nm == attr){
+        children[i].children[0].id = "active"
+        continue  
+      }
+    }catch(e){}
+
+    children[i].children[0].id = ""
+  }
+  obj.id = "active";
+  section = document.querySelector(`a[name=${nm}]`)
+  children = section.parentNode.parentNode.children;
+  i = 0
+  for (; i < children.length; i++) 
+    children[i].classList.add("hidden")
+  var cls_list = section.parentNode.classList
+  if (cls_list.contains("hidden")) cls_list.remove("hidden")
+
+  section.parentNode.classList = cls_list
+  document.getElementById("sm-menu").classList.add("hidden")
+  window.scrollTo(0,0);
 }
 function toggleMenu() {
-  var m_list = document.getElementById("sm-menu-list");
+  var m_list = document.getElementById("sm-menu");
   var cs = m_list.classList;
-  if (cs.contains("hidden")) cs.remove("hidden");
-  else cs.add("hidden");
+  if (cs.contains("hidden")){
+    cs.remove("hidden");
+    cs.add("slide-from-right")
+  }
+  else{
+    cs.remove("slide-from-right")
+    cs.add("slide-to-right")
+    setTimeout(function() {
+      cs.remove("slide-to-right")
+      cs.add("hidden")
+    }, 650);
+  }
 }
 load()
