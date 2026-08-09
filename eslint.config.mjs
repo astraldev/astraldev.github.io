435bdd562d28f14e37503dae269b3c4cff2ff16f
Ekure Edem <61796351+astraldev@users.noreply.github.com>
2026-08-09 22:34:40 +0100

    chore(deps): refresh lockfile and upgrade dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
-import stylistic from "@stylistic/eslint-plugin";
 
 export default withNuxt([
   {
-    plugins: {
-      "@stylistic": stylistic,
-    },
     rules: {
       "@stylistic/indent": ["error", 2],
       "@stylistic/semi": ["error", "always"],
